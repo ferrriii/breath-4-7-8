6448d0a4efc4526c4456d0b8945128715bf6e3eb
ferrriii <57658129+ferrriii@users.noreply.github.com>
2020-03-27 16:51:14 +0430

    Update Home.vue

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,27 +1,102 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"></Label>
-        </ActionBar>
-
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
+	<Page class="page" actionBarHidden="true" @loaded="appLoaded">
+		<FlexboxLayout flexDirection="column" backgroundColor="#36163a" justifyContent="space-around" height="100%">
+			<FlexboxLayout justifyContent="center">
+				<StackLayout class="reset" @tap="restart">
+					<Label text="restart" color="white"/>
+				</StackLayout>
+			</FlexboxLayout>		
+			<Label class="action" :text="action" textAlignment="center" ref="action"></Label>
+			<Label class="count" :text="count" key="cnt" textAlignment="center" ref="counter"></Label>			
+		</FlexboxLayout>
     </Page>
 </template>
 
 <script>
     export default {
+		data() {
+			return {
+				count: 4,
+				timer: undefined,
+				actionIndex: 0,
+				actionCounts: [
+					{name: 'Inhale', count: 4, vibration: [0, 50]},
+					{name: 'Hold', count: 7, vibration: [0, 50, 100, 50]},
+					{name: 'Exhale', count: 8, vibration: [0, 200]}
+				]
+			}
+		},
         computed: {
-            message() {
-                return "Blank {N}-Vue app";
+            action() {
+                return this.actionCounts[this.actionIndex].name
             }
-        }
+        },
+		destroyed() {
+			clearInterval(this.timer)
+		},
+		methods: {
+			onButtonTap() {
+				//vibrator.vibrate(2000); 
+				let srv = app.android.context.getSystemService(android.content.Context.VIBRATOR_SERVICE);
+				console.log(srv.hasVibrator());
+				srv.vibrate(2000); 
+			},
+			restart() {
+				clearInterval(this.timer)
+				this.timer = setInterval(this.tick, 1000);
+				this.actionIndex = 0
+				this.vibrate(this.actionCounts[this.actionIndex].vibration)
+				this.count = this.actionCounts[this.actionIndex].count;
+			},
+			appLoaded() {
+				if (!this.timer)
+					this.timer = setInterval(this.tick, 1000);
+			},
+			vibrate(param) {
+				let srv = app.android.context.getSystemService(android.content.Context.VIBRATOR_SERVICE);
+//				srv.vibrate(100);
+				if (!srv.hasVibrator())
+					return
+                // Create the pattern array
+                let pattern = Array.create('long', param.length);
+                // Assign the pattern values
+                param.forEach((value, index) => { pattern[index] = value; });
+                // Vibrate pattern
+				srv.vibrate(pattern, -1);
+			},
+			tick() {
+				this.count--
+				if (this.count == 0)
+				{
+					//switch to next action
+					this.actionIndex++
+					if (this.actionIndex >= this.actionCounts.length)
+						this.actionIndex = 0
+					
+					this.vibrate(this.actionCounts[this.actionIndex].vibration)
+						
+					this.count = this.actionCounts[this.actionIndex].count;
+			
+					let actionLayout = this.$refs.action.nativeView; // Needed to avoid masking child components on Android
+					var hide = actionLayout.createAnimation({ opacity: 0, duration: 300 });
+					var show = actionLayout.createAnimation({ opacity: 1, duration: 500 });
+					hide.play()
+						.then(function () { return show.play(); })
+	
+				}
+				
+				let counterLayout = this.$refs.counter.nativeView; // Needed to avoid masking child components on Android
+				
+				var grow = counterLayout.createAnimation({scale: { x: 1.5, y: 1.5}, duration: 10});
+				var shrink = counterLayout.createAnimation({scale: { x: 1, y: 1}, duration: 980});
+				grow.play()
+					.then(function () { return shrink.play(); })
+					.catch(function (e) {
+					console.log(e.message);
+				});
+				
+			}
+		}
     };
 </script>
 
@@ -29,13 +104,28 @@
     @import '~@nativescript/theme/scss/variables/blue';
 
     // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+	.action {
+		color: white;
+		font-size: 60vmin;
+		padding-left: 10vw;
+		padding-right: 10vw;
+		padding-top: 20vh;
+		padding-bottom: 60vh;
+	}
+	
+	.count {
+		color: white;
+		font-size: 30vmin;
+	}
+	
+	.reset {
+		width: 80vw;
+		height: 50vw;
+		border-color: white;
+		border-width: 2px;
+		border-radius: 50vw;
+		vertical-align: center;
+		text-align: center;
+		font-size:15vmin;
+	}
 </style>
